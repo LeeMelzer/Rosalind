7c3e69fe7dad44571e2b9c730cee0b0661e430f1
LeeMelzer <lee.melzer@gmail.com>
2024-09-21 14:57:19 -0700

    daily practice

--- template.py
+++ template.py
@@ -1,6 +1,8 @@
+import random
+
 def partition(arr, low, high):
     pivot = arr[high]
-    i = low - 1
+    i = low-1
 
     for j in range(low, high):
         if arr[j] <= pivot:
@@ -10,14 +12,22 @@ def partition(arr, low, high):
     (arr[i+1], arr[high]) = (arr[high], arr[i+1])
     return i+1
 
+def setRandomPivot(arr, low, high):
+    randPivot = random.randint(low, high)
+    (arr[high], arr[randPivot]) = (arr[randPivot], arr[high])
+    return partition(arr, low, high)
+
 def quickSort(arr, low, high):
     if low < high:
-        pi = partition(arr, low, high)
+        pi = setRandomPivot(arr, low, high)
         quickSort(arr, 0, pi-1)
         quickSort(arr, pi+1, high)
 
 def main():
-    arr = [5, 3, 8, 9, 6, 1, -1]
+    with open("data.txt") as f:
+        n = int(f.readline().split()[0])
+        arr = list(map(int, f.readline().split()))
+
     quickSort(arr, 0, len(arr)-1)
     print(*arr)
 
