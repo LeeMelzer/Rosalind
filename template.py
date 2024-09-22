66f3117b5409a133a0adbbbcad3ab9cc8d3ce299
LeeMelzer <lee.melzer@gmail.com>
2024-09-22 11:35:21 -0700

    daily practice

--- template.py
+++ template.py
@@ -1,35 +1,17 @@
-import random
-
-def partition(arr, low, high):
-    pivot = arr[high]
-    i = low-1
-
-    for j in range(low, high):
-        if arr[j] <= pivot:
-            i += 1
-            (arr[i], arr[j]) = (arr[j], arr[i])
-    
-    (arr[i+1], arr[high]) = (arr[high], arr[i+1])
-    return i+1
-
-def setRandomPivot(arr, low, high):
-    randPivot = random.randint(low, high)
-    (arr[high], arr[randPivot]) = (arr[randPivot], arr[high])
-    return partition(arr, low, high)
-
-def quickSort(arr, low, high):
-    if low < high:
-        pi = setRandomPivot(arr, low, high)
-        quickSort(arr, 0, pi-1)
-        quickSort(arr, pi+1, high)
+import heapq
 
 def main():
     with open("data.txt") as f:
         n = int(f.readline().split()[0])
-        arr = list(map(int, f.readline().split()))
-
-    quickSort(arr, 0, len(arr)-1)
-    print(*arr)
+        arr = list(map(lambda x: int(x) * -1, f.readline().split()))
+    
+    heapq.heapify(arr)
+    result = []
+    while arr:
+        result.append(heapq.heappop(arr)*-1)
+    
+    print(*result)
+    
 
 if __name__ == "__main__":
     main()
