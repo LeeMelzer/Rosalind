8f328e74c36f31f9cf218e974da988f5b13b39b5
LeeMelzer <lee.melzer@gmail.com>
2024-10-20 10:54:51 -0700

    daily practice

--- template.py
+++ template.py
@@ -1,15 +1,28 @@
-import heapq
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
+
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            elif self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+
+    def isConnected(self, x, y):
+        return self.find(x) == self.find(y)
+
     
-def main():
-    arr = [1, 5, 9, 0, 23, -8]
-
-    heapq.heapify(arr)
-    result = []
-
-    while arr:
-        result.append(heapq.heappop(arr))
-    
-    print(*result)
-
-if __name__ == "__main__":
-    main() 
